7cc5f73d92688b3de18a8b7a64358005d6dafa43
riku59 <123378420+riku59@users.noreply.github.com>
2024-01-22 16:55:06 +0100

    formulaire de contact, a finaliser

--- Front/src/Components/FormContact.js
+++ Front/src/Components/FormContact.js
@@ -4,6 +4,13 @@ const FormContact = () => {
   return (
     <div className="form-contact">
       <h1>me contacter</h1>
+      <form action="">
+        <label htmlFor="email">E-mail</label>
+        <input type="email" name="email" id="email" />
+        <label htmlFor="password">Mot de passe</label>
+        <input type="password" name="password" id="password" />
+        <input type="submit" />
+      </form>
     </div>
   );
 };

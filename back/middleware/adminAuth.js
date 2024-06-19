e301a53235e47240d90d80a5dd5842b45c95b33e
riku59 <123378420+riku59@users.noreply.github.com>
2024-06-19 18:47:56 +0200

    mise en place de l'edit , suppression , et post dans la base de donnée

--- back/middleware/adminAuth.js
+++ back/middleware/adminAuth.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/user.model");
 
+// Vérifier que l'utilisateur est admin
 module.exports = async (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1]; // Récupère le token du header Authorization
